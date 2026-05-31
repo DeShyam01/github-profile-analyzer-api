240dbaad28a24df2dc2b8a90e5f9718131efc686
DeShyam01 <shyamsundargayen345@gmail.com>
2026-05-31 12:08:32 +0530

    added rate limitter

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const cors = require('cors');
 const pool = require('./src/config/db');
 const app = express();
 const githubRoutes = require('./src/routes/githubRoutes');
+const { limiter, securityHeaders } = require('./src/middleware/security');
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(securityHeaders);
+app.use(limiter);
 app.use('/api/github', githubRoutes);
 
 app.listen(PORT, () => {
